eee7bd477454db9ae0b9383f7cfbf32e5227e5c9
Kielley Young <kielleyyoung@Kielleys-MacBook-Pro.local>
2020-11-24 17:05:05 -0500

    updated files so query progression broken out across files

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -93,3 +93,18 @@ SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Using the count function 
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Generating an output where result is table *instead of list as it was above
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
